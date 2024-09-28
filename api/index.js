e826bda72fdeee55b23e558f5fb8ac66edab43fe
loneltnitzz <vasanthmurugesan47@gmail.com>
2024-09-28 08:03:23 +0530

    service

---
+++ api/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.get("/home", (req, res) => res.send("welcome"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
